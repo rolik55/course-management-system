fe410e72ec32627aac652e1efde45db29d6b03d8
Rolandas <romanovskisrolandas@gmail.com>
2022-12-02 14:06:08 +0200

    moderator unit tests

---
+++ src/test/java/com/coursemngsys/coursemanagementsystem/Model/ModeratorTest.java
@@ -0,0 +1,28 @@
+package com.coursemngsys.coursemanagementsystem.Model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ModeratorTest {
+
+    @Test
+    void getModeratedCourses() {
+    }
+
+    @Test
+    void setModeratedCourses() {
+    }
+
+    @Test
+    void removeModeratedCourse() {
+    }
+
+    @Test
+    void getEditableFolders() {
+    }
+
+    @Test
+    void setEditableFolders() {
+    }
+}
